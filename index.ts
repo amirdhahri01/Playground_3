3d2ebc05822293d804dff28305065f371916cf28
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-01 23:27:19 +0100

    An update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const menu = [{name:"Margherita" , price:8},
               {name:"Pepperoni" , price:10},
               {name:"Hawaiian" , price:10},
               {name:"Veggie" , price:9}]
-const cashInRegister = 100
+let cashInRegister = 100
 let nextOrderId = 1;
 const orderQueue = []
 
